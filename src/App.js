7f2417db5d6e7242dccb5258d6c04fc60423ed52
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-14 01:19:22 +0600

    sign up and sign in toggle button with condition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import "./App.css";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "./firebase.init";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         console.log(result.user);
+        setUser(result.user);
       })
       .catch((error) => {
         console.error("error", error);
       });
   };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
+  };
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {/* {condition ? true : false} */}
+      {user.email ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      )}
+
+      <h2>Name:{user.displayName}</h2>
+      <p>Email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
