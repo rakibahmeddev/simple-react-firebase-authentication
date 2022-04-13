d1d56db9e62deab1fcae2a6a348f900516721f6d
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-13 21:49:14 +0600

    simple login popup

---
+++ src/_steps.js
@@ -0,0 +1,36 @@
+/*
+* Steps to use firebase
+* 1. Create a project on console.firebase.google.com 
+* 2. Install firebase
+* 3. Register web app in firebase
+* 4. Copy firebase init with config from firebase project settings
+* 5. export default app
+* 6. import app firebase.init.js into your app.js
+* 7. import getAuth from firebase/Auth and create auth = getAuth(app)
+* 8. turn on google authentication 
+* 9. Create Google Provide
+* 10. Sig
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCo3gPxi6Is3NQ0gzjttWyk_mJZpyR9i2o",
+  authDomain: "simple-react-firebase-146c0.firebaseapp.com",
+  projectId: "simple-react-firebase-146c0",
+  storageBucket: "simple-react-firebase-146c0.appspot.com",
+  messagingSenderId: "855500613319",
+  appId: "1:855500613319:web:f1d189c805b05a529300e2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
